86223949dcfd3e9978d4c8e46dcd6fbde3263ea1
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-02 19:21:33 -0300

    Creates storage property

--- scripts.js
+++ scripts.js
@@ -12,3 +12,13 @@ const modal = {
             .remove('active');
     }
 };
+
+const storage = {
+    get() {
+        return JSON.parse(localStorage.getItem("dev.finances:transactions")) || [];
+    },
+
+    set(transactions) {
+        localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions));
+    }
+};
