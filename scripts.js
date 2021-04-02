5d84778b0b97f247bf9b4caa4fa0b67c38c45e7d
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-02 19:27:16 -0300

    Creates Transaction object

--- scripts.js
+++ scripts.js
@@ -22,3 +22,45 @@ const storage = {
         localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions));
     }
 };
+
+const transaction = {
+    all: storage.get(),
+
+    add(transaction) {
+        transaction.all.push(transaction);
+
+        App.reload();
+    },
+
+    remove(index) {
+        transaction.all.splice(index, 1);
+
+        App.reload();
+    },
+
+    incomes() {
+        let income = 0;
+        transaction.all.forEach(transaction => {
+            if (transaction.value > 0) {
+                income += transaction.value;
+            }
+        });
+
+        return income;
+    },
+
+    expenses() {
+        let expense = 0;
+        transaction.all.forEach(transaction => {
+            if (transaction.value < 0) {
+                expense += transaction.value;
+            }
+        });
+
+        return expense;
+    },
+
+    total() {
+        return transaction.incomes() + transaction.expenses();
+    }
+};
